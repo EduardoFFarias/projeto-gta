211c299e11a4895eab48c0c1b9be067d9d68627d
EduardoFFarias <eduardofarias592@gmail.com>
2024-01-28 11:55:10 -0300

    alteraçoes css

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ const botao = document.querySelector('.btn-plataforma');
 const elementoPlataforma = document.querySelector('.btn-plataforma .plataforma')
 
 botao.addEventListener("click", () => {
-    
+
     elementoPlataforma.classList.toggle("ativo")
 
 })
+
